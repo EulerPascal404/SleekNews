284249140da412985119306597d6b87340369c70
Joseph Jiang <josephjiang2006@gmail.com>
2022-06-25 22:16:50 -0500

    poopta

--- SleekApp/app.py
+++ SleekApp/app.py
@@ -10,10 +10,19 @@ from flask import Response
 import spacy
 import nltk
 from bs4 import BeautifulSoup
-
+import torch
 from collections import Counter
+import pandas as pd
 
 os.environ["TOKENIZERS_PARALLELISM"] = "true"
+tokenizer = AutoTokenizer.from_pretrained("distilroberta-base")
+model = AutoModel.from_pretrained("SleekApp/checkpoint-7800")
+data = pd.read_csv("SleekApp/dataset_first_100000.csv")
+embeddings = np.load("SleekApp/news_embeddings (1).npy")
+def PrepareExample(text):
+    inputs = tokenizer(text, truncation=True, max_length=512, padding="max_length")
+    inputs = {k : torch.tensor(v).unsqueeze(0) for (k, v) in inputs.items()}
+    return inputs
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk import tokenize
@@ -51,6 +60,16 @@ def SentenceWordImportance(text, num_display):
 ur = "https://devonprice.medium.com/thoughts-are-not-feelings-is-shitty-psychological-advice-b5a292d0a7f1"
 # print(finalKeyWords(ur, 3)[1])
 # print(GetEntities(getText(ur)))
+def GetMostSimilar(url):
+    text = getText(url)
+    example = PrepareExample(text)
+    example = {k : v for (k, v) in example.items()}
+    with torch.no_grad():
+        text_embed = model(**example)[1].squeeze().detach().cpu().numpy()
+    similarity_scores = np.zeros(len(embeddings))
+    for i in range(len(embeddings)):
+        similarity_scores[i] = np.corrcoef(text_embed, embeddings[i])[0][1]
+    return data["Content"].values[np.argmax(similarity_scores)]
 @app.route("/")
 @app.route("/home")
 def home():
@@ -60,7 +79,7 @@ def home():
 def result():
 	content = request.form.to_dict()
 	name1 = content["name"]
-	return render_template("index.html",inp = SentenceWordImportance(getText(name1),3))
+	return render_template("index.html",inp = [SentenceWordImportance(getText(name1),3),0,GetMostSimilar(name1)])
 
 if __name__ == '__main__':
 	app.run(debug = True,port = 5001)
